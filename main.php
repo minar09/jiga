a2c21a055b18ab9a861f7c5a0ec4ae2a84860c86
minar09 <minar09.bd@gmail.com>
2013-11-13 13:13:47 +0600

    polapiner khobor ki?

--- main.php
+++ main.php
@@ -1,4 +1,18 @@
 <?php
-    echo 'Hello jiga!';
+    require_once 'google/appengine/api/users/UserService.php';
+
+    use google\appengine\api\users\User;
+    use google\appengine\api\users\UserService;
+
+    $user = UserService::getCurrentUser();
+
+    if ($user) {
+        echo 'Hello, ' . htmlspecialchars($user->getNickname());
+    }
+    else {
+      header('Location: ' .
+             UserService::createLoginURL($_SERVER['REQUEST_URI']));
+    }
+
 	
 	
